e2643aaf92bdda85e829b952ed44006ccb9266ff
anturchin <tur4intwo@gmail.com>
2023-12-30 14:22:50 +0300

    refactor(extended-1.2): use media alternatives

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.classList.add("todo-button", "delete");
   deleteButtonImg.src="./remove.svg";
+  deleteButtonImg.alt = "Delete";
   deleteButtonImg.classList.add("todo-icon");
   deleteButton.appendChild(deleteButtonImg);
 
